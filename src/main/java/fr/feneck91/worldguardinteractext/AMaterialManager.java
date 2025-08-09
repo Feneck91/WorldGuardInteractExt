f40147074d67506ac13157a9b2da1871c60b7da3
Feneck91 <schateau@laposte.net>
2025-08-09 23:56:50 +0200

    Add MaterialInformation class that allow to add more properties to blocks, like title or author on book.

--- src/main/java/fr/feneck91/worldguardinteractext/AMaterialManager.java
+++ src/main/java/fr/feneck91/worldguardinteractext/AMaterialManager.java
@@ -20,6 +20,83 @@ import java.util.regex.Pattern;
 public abstract class AMaterialManager  implements IMaterialManager
 {
     /**
+     * Class used from derived class to record material information with more properties.
+     */
+    protected static class MaterialInformation
+    {
+        /**
+         * Material.
+         */
+        private final Material m_material;
+
+        /**
+         * Contains properties for this material like color, text, author, depending on material type.
+         */
+        private final Map<String, String> m_dicProperties;
+
+        /**
+         * Constructor.
+         *
+         * @param _material Material.
+         * @param _dicProperties Extra properties, can be null.
+         */
+        public MaterialInformation(Material _material, Map<String, String> _dicProperties)
+        {
+            m_material = _material;
+            m_dicProperties = _dicProperties == null ? new HashMap<String, String>() : _dicProperties;
+        }
+
+        /**
+         * Get the material.
+         *
+         * @return The material.
+         */
+        public Material getMaterial()
+        {
+            return m_material;
+        }
+
+        /**
+         * Get a map with extended properties.
+         *
+         * @return All the properties.
+         */
+        public Map<String, String> getProperties()
+        {
+            return m_dicProperties;
+        }
+
+        /**
+         * Convert to string.
+         *
+         * @return A string that represent the class content.
+         */
+        @Override
+        public String toString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.append(m_material.name());
+            if (!getProperties().isEmpty())
+            {
+                sb.append(" (");
+                boolean bAddComma = false;
+                for (Map.Entry<String, String> entryProperties : getProperties().entrySet())
+                {
+                    sb.append(bAddComma ? ", " : "")
+                      .append(entryProperties.getKey())
+                      .append(" : \"")
+                      .append(entryProperties.getValue())
+                      .append("\"");
+                    bAddComma = true;
+                }
+                sb.append(")");
+            }
+
+            return sb.toString();
+        }
+    };
+
+    /**
      * Instance of plugin.
      */
     private final WorldGuardInteractExt m_plugin;
@@ -42,6 +119,148 @@ public abstract class AMaterialManager  implements IMaterialManager
     public WorldGuardInteractExt getPlugin()
     {
         return m_plugin;
+    }
+
+    /**
+     * Get a list of materials from a name.
+     *
+     * @param _strKeyName Key name to retrieve the list of materials informations (name / regex and extra informations).
+     * @param _mapItems Map where to find informations.
+     * @param _lambdaIsMaterialInformationValid Lambda to check if the material information is valid for this search.
+     * @param _lambdaInfoAdd Lambda to display log that this material is added.
+     * @param _lambdaInfoInvalid Lambda to display log that this material is not added (but should be).
+     * @return An allowed material informations list.
+     */
+    protected List<MaterialInformation> findMaterialsX(String _strKeyName, Map<String, Object> _mapItems, Function<MaterialInformation, Boolean> _lambdaIsMaterialInformationValid, Consumer<MaterialInformation> _lambdaInfoAdd, Consumer<MaterialInformation> _lambdaInfoInvalid)
+    {
+        if (_lambdaIsMaterialInformationValid == null)
+        {
+            throw new NullPointerException("AMaterialManager::findMaterials : _lambdaIsMaterialInformationValid argument is null");
+        }
+        ArrayList<Object> listMaterialInformations = (ArrayList<Object>) _mapItems.get(_strKeyName);
+        List<MaterialInformation> listMaterialsInformations = new ArrayList<MaterialInformation>();
+        Map<String, String> dicProperties = new HashMap<String, String>();
+
+        // Read all materials
+        if (listMaterialInformations.isEmpty())
+        {   // Empty list, take all material that are valid
+            for (Material itemMaterial : Material.values())
+            {
+                MaterialInformation materialInformation = new MaterialInformation(itemMaterial, null);
+                if (_lambdaIsMaterialInformationValid.apply(materialInformation))
+                {
+                    listMaterialsInformations.add(materialInformation);
+                    if (getPlugin().isVerboseLogEnabled() && _lambdaInfoAdd != null)
+                    {
+                        _lambdaInfoAdd.accept(materialInformation);
+                    }
+                }
+            }
+        }
+        else
+        {
+            Material itemMaterialFound = null;
+            String strItemName = null; // Name or regex
+
+            for (Object oItem : listMaterialInformations)
+            {
+                if (oItem instanceof String strItemNameToFind)
+                {   // Only a material name
+                    itemMaterialFound = Material.getMaterial(strItemNameToFind);
+                    strItemName = strItemNameToFind;
+                }
+                else if (oItem instanceof HashMap<?, ?> dicItemsToCheck)
+                {   // Material name with others values
+                    if (   dicItemsToCheck.keySet().stream().allMatch(k -> k instanceof String)
+                        && dicItemsToCheck.values().stream().allMatch(v -> v instanceof String))
+                    {   // All keys / values are String
+                        HashMap<String, String> dicItems = (HashMap<String, String>) dicItemsToCheck;
+                        if (dicItems.containsKey("material"))
+                        {
+                            strItemName = dicItems.get("material");
+                            itemMaterialFound = Material.getMaterial(strItemName);
+
+                            for (Map.Entry<String, String> entryProperties : dicItems.entrySet())
+                            {
+                                if (!entryProperties.getKey().equals("material"))
+                                {
+                                    if (!dicProperties.containsKey(entryProperties.getKey()))
+                                    {
+                                        dicProperties.put(entryProperties.getKey(), entryProperties.getValue());
+                                    }
+                                    else
+                                    {
+                                        getPlugin().getLogger().warning("Configuration " + getMaterialType() + ": more than one key '" + entryProperties.getKey() +"' is filled, only first is keep!");
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            getPlugin().getLogger().warning("Configuration " + getMaterialType() + ": bad configuration for key '" +  _strKeyName + "', subkey 'material' is mandatory for dictionary informations, configuration is ignored!");
+                            listMaterialsInformations = null;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        getPlugin().getLogger().warning("Configuration " + getMaterialType() + ": bad configuration for key '" +  _strKeyName + "', configuration ignored!");
+                        listMaterialsInformations = null;
+                        break;
+                    }
+                }
+                else
+                {
+                    getPlugin().getLogger().warning("Configuration " + getMaterialType() + ": bad configuration for key '" +  _strKeyName + "', configuration ignored!");
+                    listMaterialsInformations = null;
+                    break;
+                }
+
+                if (itemMaterialFound == null)
+                {   // Not found, try to find with regex
+                    Pattern pattern = Pattern.compile(strItemName); // CASE_SENSITIVE
+
+                    for (Material itemMaterial : Material.values())
+                    {
+                        if (pattern.matcher(itemMaterial.name()).find())
+                        {
+                            MaterialInformation materialInformation = new MaterialInformation(itemMaterial, dicProperties);
+                            if (_lambdaIsMaterialInformationValid.apply(materialInformation))
+                            {
+                                listMaterialsInformations.add(materialInformation);
+                                if (getPlugin().isVerboseLogEnabled())
+                                {
+                                    _lambdaInfoAdd.accept(materialInformation);
+                                }
+                            }
+                            else
+                            {   // Not valid, ignore it
+                                _lambdaInfoInvalid.accept(new MaterialInformation(itemMaterial, null));
+                            }
+                        }
+                        // else : ignore it
+                    }
+                }
+                else
+                {
+                    MaterialInformation materialInformation = new MaterialInformation(itemMaterialFound, dicProperties);
+                    if (_lambdaIsMaterialInformationValid.apply(materialInformation))
+                    {
+                        listMaterialsInformations.add(materialInformation);
+                        if (getPlugin().isVerboseLogEnabled())
+                        {
+                            _lambdaInfoAdd.accept(materialInformation);
+                        }
+                    }
+                    else
+                    {   // Not valid, ignore it
+                        _lambdaInfoInvalid.accept(materialInformation);
+                    }
+                }
+            }
+        }
+
+        return listMaterialsInformations;
     }
 
     /**
